753ecf0dbabaebdb21919142f047cf914b252ed1
Morten Noddeland <morten@noddeland.no>
2020-07-08 21:11:41 +0200

    Setup components folder

---
+++ components/text.js
@@ -0,0 +1,5 @@
+export default function Text({ children}) {
+  return (
+    <p>{ children}</p>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Text from '../components/text';
+
 export default function IndexPage() {
   return (
-    <h1>Hello Trondheim!</h1>
+    <>
+      <h1>Hello Trondheim!</h1>
+      <Text>Hope you're all doing well.</Text>
+    </>
   );
 }

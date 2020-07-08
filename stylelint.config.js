4233a85e8cd674282c60ab848c142eacee3ecab6
Morten Noddeland <morten@noddeland.no>
2020-07-09 01:25:53 +0200

    Stylelint: Make stylelint play nice with Tailwind

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   extends: ['stylelint-config-standard'],
+  rules: {
+    // Add exceptions to support Tailwind
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+  },
 };
